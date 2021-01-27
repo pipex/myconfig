cbb51ac6a8eace8cd4396d747a5f5f8fe3df6b86
Felipe Lalanne <felipe@balena.io>
2021-01-27 11:08:37 -0300

    Add first commit for balena oh-my-zsh plugin

---
+++ oh-my-zsh/custom/plugins/balena/balena.plugin.zsh
@@ -0,0 +1,6 @@
+# TODO: change the prompt to show
+# * current signed backend (balena-cloud, balena-staging, your own). Get from balenarc.yml or env vars
+# * login status (green for logged in)
+# * signed in user (maybe admin privileges)
+
+# echo "hello"
