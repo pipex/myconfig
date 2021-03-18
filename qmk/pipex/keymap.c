4677afdb59467fc47d753ed36128e6ab67b3f97e
Felipe Lalanne <felipe@balena.io>
2021-03-18 19:47:02 -0300

    Move volume control to lower layer on keymap

--- qmk/pipex/keymap.c
+++ qmk/pipex/keymap.c
@@ -68,15 +68,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |  >=  |  <<  |  >>  |      |      |             |      | Mute |VolDn |VolUp |Print |
+ * | Lock |Print |      |      |      |             |      | Home | PgDn | PgUp | End  |
  * `-----------------------------------------------------------------------------------'
  * 
  */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_GRV ,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
-    _______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR,  KC_ENT,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_RSFT,
-       LOCK,   PRINT, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP,  KC_END 
+    KC_GRV ,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,         KC_9,      KC_0, KC_BSPC,
+    _______, _______, _______, _______, _______, _______, _______, KC_UNDS, KC_PLUS,      KC_LCBR,   KC_RCBR,  KC_ENT,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______,      _______,   _______, KC_RSFT,
+    KC_MPLY, KC_MPRV, KC_MNXT, _______, _______, _______, _______, _______,  _______, KC__VOLDOWN, KC__VOLUP, KC_MUTE
 ),
 
 /* Raise
@@ -87,15 +87,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |      |   "  |   '  |  \   |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Lock |      |      |      |      |             |      | Home | PgDn | PgUp | End  |
+ * |  >=  |  <<  |  >>  |      |      |             |      | Mute |VolDn |VolUp |      |
  * `-----------------------------------------------------------------------------------'
  * 
  */
 [_RAISE] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR,     KC_LPRN,   KC_RPRN, KC_BSPC,
-    _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, KC_MINS,   KC_EQL,     KC_LBRC,   KC_RBRC, KC_PIPE,
-    _______,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,  KC_DQUO,     KC_QUOT,   KC_BSLS, KC_RSFT,
-    KC_MPLY, KC_MPRV, KC_MNXT, _______, _______, _______, _______, _______,  _______, KC__VOLDOWN, KC__VOLUP, KC__MUTE
+    KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
+    _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6, KC_MINS,   KC_EQL, KC_LBRC, KC_RBRC, KC_PIPE,
+    _______,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, _______,  KC_DQUO, KC_QUOT, KC_BSLS, KC_RSFT,
+       LOCK,   PRINT, _______, _______, _______, _______, _______, _______,  KC_HOME, KC_PGDN, KC_PGUP, KC_END 
 ),
 /* Numpad
  * ,-----------------------------------------------------------------------------------.
