f38024d536f7524cbbc914ae6ae536f23e9d5878
Felipe Lalanne <felipe@balena.io>
2022-03-13 15:29:33 -0300

    Add blink shell theme

---
+++ catpuccin-blink.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#6E6C7E", "#F28FAD", "#ABE9B3", "#FAE3B0", "#96CDFB", "#F5C2E7", "#89DCEB", "#D9E0EE", "#988BA2", "#F28FAD", "#ABE9B3", "#FAE3B0", "#96CDFB", "#F5C2E7", "#89DCEB", "#D9E0EE"]);
+t.prefs_.set('foreground-color', "#D9E0EE");
+t.prefs_.set('background-color', "#1E1E2E");
+t.prefs_.set('cursor-color', '#F5E0DC');
